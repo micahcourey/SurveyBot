aad6d2ab5ca69026fb7372a001f4c37c105a60cf
micahcourey <micahjcourey@gmail.com>
2015-09-27 14:03:29 -0700

    add routes to app.js

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-var surveyBot = angular.module('surveyBot', []);
+var surveyBot = angular.module('surveyBot', ['ui.router']);
+
+surveyBot.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    views: {
+      'header': {
+        templateUrl: "partials/header.html",
+        controller: "HeadersCtrl"
+      },
+      'body': {
+        templateUrl: "partials/home.html"
+      }
+    }
+  });
+
+  $stateProvider.state('questions', {
+    url: "/questions",
+    views: {
+      'header': {
+        templateUrl: "partials/header.html",
+        controller: "HeadersCtrl"
+      },
+      'body': {
+        templateUrl: "partials/questions.html",
+        controller: "QuestionsCtrl"
+      }
+    }
+  });
+
+  $stateProvider.state('survey', {
+    url: "/survey",
+    views: {
+      'header': {
+        templateUrl: "partials/header.html",
+        controller: "HeadersCtrl"
+      },
+      'body': {
+        templateUrl: "partials/survey.html",
+        controller: "ResponsesCtrl"
+      }
+    }
+  });
+});
