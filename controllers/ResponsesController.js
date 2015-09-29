7d2f2cbc156d0af957e35bb754a14bf5ca979f61
micahcourey <micahjcourey@gmail.com>
2015-09-29 10:14:27 -0700

    add form to survey and UtilitiesFactory for question id's

--- controllers/ResponsesController.js
+++ controllers/ResponsesController.js
@@ -1,4 +1,16 @@
 surveyBot.controller('ResponsesCtrl', function ResponsesCtrl($scope, QuestionsFactory) {
-  $scope.responses = QuestionsFactory.responses;
-  $scope.QuestionsFactory = QuestionsFactory;
+
+  $scope.question = UtilitiesFactory.findById(QuestionsFactory.questions, $stateParams.questionId)
+
+  $scope.addResponse = function() {
+    $scope.question.response.push({ response: $scope.questionResponse });
+    $scope.questionResponse = null;
+  };
+});
+
+courseRoster.controller("StudentCtrl", function ($scope) {
+  $scope.addResponse = function(questionResponse) {
+    $scope.question.responses.push({ response: questionResponse });
+    $scope.questionResponse = null;
+  }
 });
--- services/QuestionsFactory.js
+++ services/QuestionsFactory.js
@@ -1,18 +1,12 @@
 surveyBot.factory('QuestionsFactory', function QuestionsFactory() {
   var factory = {};
   factory.questions = [];
-  factory.responses = [];
 
   factory.addQuestion = function() {
-    factory.questions.push({ question: factory.question });
+    factory.questions.push({ question: factory.question, id: factory.questions.length + 1, responses: []  });
     factory.question = null;
   };
 
-  factory.addResponse = function() {
-    var response = { response: factory.response };
-    factory.responses.push(responses);
-    factory.responses = null;
-  };
 
   return factory;
 });
